6a3bd8301f53289165b776f16918eeaca9f26d94
sungjaeHong <sungjae89@gmail.com>
2016-09-28 14:27:00 +0900

    Update QuickSort.java
    단계별 pick, swap 로그에 남김

--- QuickSort/src/com/tistory/devhong/QuickSort.java
+++ QuickSort/src/com/tistory/devhong/QuickSort.java
@@ -11,19 +11,24 @@ public class QuickSort {
 		}
 		int arr2[] = {3, 7, 5, 1, 6, 2, 4};
 		System.out.println("정렬 전");
-		printArr(arr);
+		printArr(arr2);
 		quickSort(arr2, 0, arr2.length-1);
 		System.out.println("정렬 후");
-		printArr(arr);
+		printArr(arr2);
 		
 
 	}
 	public static void quickSort(int arr[], int left, int right){
+//		System.out.println("do quickSort");
 		int partition = partition(arr, left, right);
-		if(left<partition-1)
+		if(left<partition-1){
+			//System.out.println("left : " + left + " right : " + (partition-1));
 			quickSort(arr, left, partition-1);
-		if(partition<right)
+		}
+		if(partition<right){
+			//System.out.println("left : " + partition + " right : " + right);
 			quickSort(arr,partition,right);
+		}
 	}
 	
 	public static int partition(int arr[], int low, int high){
@@ -36,22 +41,26 @@ public class QuickSort {
 			while(arr[i] < pivot){
 				i++;
 			}
+			System.out.println("picked i arr[" + i + "] : " + arr[i]);
 			while(arr[j] > pivot){
 				j--;
 			}
+			System.out.println("picked j arr[" + j + "] : " + arr[j]);
 			if(i<=j){
 				temp = arr[i];
 				arr[i] = arr[j];
 				arr[j] = temp;
+				System.out.println("swap " + arr[i] +", " + arr[j]);
 				i++;
 				j--;
 			}
+			printArr(arr);
 		}
 		System.out.println("return : " + i);
 		return i;
 	}
 	public static void printArr(int[] arr){
-		for(int i : arr){
+		for(int i=0;i<arr.length;i++){
 			System.out.print(arr[i]+ " ");
 		}
 		System.out.println();
