0e28478eb50e5ed9d080c5f35465721123890e68
sungjaeHong <sungjae89@gmail.com>
2016-10-04 18:23:38 +0900

    indent 수정

--- QuickSort/src/com/tistory/devhong/QuickSort.java
+++ QuickSort/src/com/tistory/devhong/QuickSort.java
@@ -19,28 +19,26 @@ public class QuickSort {
 	}
 	
 	public static void quickSort(int[] arr, int l, int r){
-        int left = l;
-        int right = r;
-        int pivot = arr[(l+r)/2];
-       
-        while (left <= right){						//왼쪽에서 비교하는 index와 오른쪽에서 비교하는 index가 역전되지않도록
-            while(arr[left] < pivot) left++;		//피벗보다 큰 값을 왼쪽부터 찾아나감
-            while(arr[right] > pivot) right--;		//피벗보다 작은값을 오른쪽부터 찾아나감
-            if(left <= right){    					//left와 right에서 찾은 값을 서로 바꿈
-                int temp = arr[left];
-                arr[left] = arr[right];
-                arr[right] = temp;
-                left++;
-                right--;
-                printArr(arr);
-            }
+		int left = l;
+		int right = r;
+		int pivot = arr[(l+r)/2];
+
+		while (left <= right){						//왼쪽에서 비교하는 index와 오른쪽에서 비교하는 index가 역전되지않도록
+		    while(arr[left] < pivot) left++;				//피벗보다 큰 값을 왼쪽부터 찾아나감
+		    while(arr[right] > pivot) right--;				//피벗보다 작은값을 오른쪽부터 찾아나감
+		    if(left <= right){    					//left와 right에서 찾은 값을 서로 바꿈
+			int temp = arr[left];
+			arr[left] = arr[right];
+			arr[right] = temp;
+			left++;
+			right--;
+			printArr(arr);
+		    }
             
-        }											//왼쪽비교값과 오른쪽비교값이 교차하면 정지
-        if(l < right) {
-        	quickSort(arr, l, left-1);	}			//바꾼 index를 기반으로 부분배열을 인자로 하여 재정렬
-        if(r > left){
-        	quickSort(arr, left, r);				//바꾼 index를 기반으로 부분배열을 인자로 하여 재정렬
-        }
+        }									//왼쪽비교값과 오른쪽비교값이 교차하면 정지
+        if(l < right) quickSort(arr, l, left-1);				//바꾼 index를 기반으로 부분배열을 인자로 하여 재정렬
+        if(r > left) quickSort(arr, left, r);					//바꾼 index를 기반으로 부분배열을 인자로 하여 재정렬
+        
     }
 
 
